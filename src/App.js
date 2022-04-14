b268b1dead8b43e52618774fd887c628a184a008
potapen <po@ta.pen>
2022-04-14 14:39:06 +0200

    routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Challenges from './pages/Challenges'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Challengr!!!!!!!!</h1>
+      <Router>
+        <Routes>
+        <Route exact path="/Challenges" element={<Challenges/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
