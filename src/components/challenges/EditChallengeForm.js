927184a8168a444dcd40cdfc608e7b41b4a9cb5f
potapen <po@ta.pen>
2022-04-15 15:09:40 +0200

    preselected league

--- src/components/challenges/EditChallengeForm.js
+++ src/components/challenges/EditChallengeForm.js
@@ -41,7 +41,9 @@ const EditChallengeForm = () => {
                   Authorization: `Bearer ${authToken}`,
                 },
               });
-            setChallenge(response.data);
+            await setChallenge(response.data);
+
+            setLeagueId(response.data.challenge.league._id)
         }
         getLeagues();
         getGames();
@@ -69,8 +71,8 @@ what we need to send
             game: gameId,
             contenders: contendersId
         }
-        console.log('challenge', challenge);
-        console.log('challengeToCreate', challengeToCreate)
+        
+        console.log('challengeToCreate', challengeToCreate);
 
         if(contendersId.length===0){setMessage('you must select contenders')}
         if(!gameId){setMessage('you must select a game')}
@@ -100,18 +102,27 @@ what we need to send
             if(e.target[i].selected){selectedContendersId.push(e.target[i].value)}
         }
         setContendersId(selectedContendersId)
-        
-        
     }
 
+    const options = [
+        { value: 'chocolate', label: 'Chocolate' },
+        { value: 'strawberry', label: 'Strawberry' },
+        { value: 'vanilla', label: 'Vanilla' }
+    ]
+    const MyComponent = () => (
+        <Select options={options} />
+    )
     return(
         <>
             <h1>Edit Challenge</h1>
             <p> {message} </p>
             <form onSubmit={submitCreate}>
                 <div>
+                    <MyComponent/>
+                </div>
+                <div>
                     <label htmlFor="league">league</label>
-                    <select id="league" onChange={leagueOnChange} defaultValue={challenge?.challenge?.league?.name}>
+                    <select id="league" onChange={leagueOnChange} value={leagueId}>
                         <option>---league select ---</option>
                         {/* when leagues is empty */}
                         {(Object.keys(leagues).length > 0) && (Array.isArray(leagues.leagues)) &&
@@ -119,25 +130,13 @@ what we need to send
                             {
                                 if(Object.keys(challenge).length > 0){
                                     return <option value={league._id}>{league.name}</option>
+                                    if(challenge.challenge.league._id === league._id){
+                                    }
                                 }
                             }
                         )
                         }
                         
-                    </select>
-                </div>
-                <div>
-
-                </div>
-                <div>
-                    <label htmlFor="game">game</label>
-                    <select id="game" onChange={gameOnChange}>
-                        <option>---game select ---</option>
-                        {games?.games && games.games.map(game =>
-                        {
-                            return <option value={game._id}>{game.name}</option>
-                        }
-                        )}
                     </select>
                 </div>
 
