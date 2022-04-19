64df4110eb160fefb7f40c62cebe1e677d8b45d3
potapen <po@ta.pen>
2022-04-19 14:52:03 +0200

    anh graphs

---
+++ src/components/stats/RadarGraph.js
@@ -0,0 +1,95 @@
+import { Radar } from 'react-chartjs-2';
+import { useState, useEffect } from "react";
+import Chart from 'chart.js/auto';
+import axios from 'axios';
+import backendHost from '../../utils/backendHost';
+import authToken from '../../utils/authToken';
+
+
+const RadarGraph = ({leagueId,userId}) => {
+    //for props of the chart components. We will need to add labels and data
+    const storedToken = localStorage.getItem("authToken");
+    const [radarData, setRadarData] = useState({
+    labels: [],
+    datasets: [
+        {
+        label: "Total score per game",
+        data: [],
+        fill: false,
+        borderColor: "rgb(75, 192, 192)",
+        tension: 0.1,
+        },
+    ],
+    });
+    async function getStakePerGameForAGivenUserAndLeague() {
+        const response = await axios.get(`${backendHost}/api/stats/radarChart/league/${leagueId}/user/${userId}`, {
+            headers: {
+                Authorization: `Bearer ${storedToken}`,
+            },
+            });
+
+        const pointsPerGameObjArray = response.data;
+        console.log('pointsPerGameObjArray', pointsPerGameObjArray)
+/*[
+    {
+        "_id": "625d61afd9cecbc3669c17cd",
+        "name": "Beer pong",
+        "description": "Le jeu du beerpong, classique",
+        "imageUrl": "https://www.jeux-alcool.com/wp-content/uploads/2017/03/beerPong.jpeg",
+        "__v": 0,
+        "createdAt": "2022-04-18T13:03:43.340Z",
+        "updatedAt": "2022-04-18T13:03:43.340Z",
+        "totalPoints": 3,
+        "totalGames": 1
+    },
+...
+    {
+        "_id": "625d61afd9cecbc3669c17d0",
+        "name": "Rock Scissors Paper",
+        "description": "The classic one",
+        "imageUrl": "https://cdn-europe1.lanmedia.fr/var/europe1/storage/images/europe1/international/la-recette-pour-gagner-a-pierre-feuille-ciseaux-768904/15409112-1-fre-FR/La-recette-pour-gagner-a-pierre-feuille-ciseaux.jpg",
+        "__v": 0,
+        "createdAt": "2022-04-18T13:03:43.341Z",
+        "updatedAt": "2022-04-18T13:03:43.341Z",
+        "totalPoints": 0,
+        "totalGames": 0
+    }
+]
+*/
+        const radarLabelsArray = [];
+        const radarDatasArrayLine = [];
+
+        pointsPerGameObjArray.forEach((obj) => {
+            radarLabelsArray.push(obj.name);
+            radarDatasArrayLine.push(obj.totalPoints);
+            });
+        
+        const updatedDataSet = [
+            {
+            label: "League Activity",
+            data: radarDatasArrayLine,
+            fill: false,
+            borderColor: "rgb(75, 192, 192)",
+            tension: 0.1,
+            },
+        ];
+        const updatedRadarData = {
+            ...radarData,
+            labels : radarLabelsArray,
+            datasets : updatedDataSet
+        }
+        console.log('updatedRadarData', updatedRadarData)
+        setRadarData(updatedRadarData)
+    }
+    useEffect(() => {
+        getStakePerGameForAGivenUserAndLeague()
+      }, []);
+
+    return(
+        <>
+            <Radar data={radarData} options={{}} />     
+        </> 
+    )
+}
+
+export default RadarGraph
