46e7fb2f4368e344110defa420bfb8e9ffe4c487
potapen <po@ta.pen>
2022-04-20 17:17:58 +0200

    variable for backend

--- src/utils/backendHost.js
+++ src/utils/backendHost.js
@@ -1,3 +1,3 @@
-const backendHost = "http://localhost:5005";
-
+const backendHost = process.env.REACT_APP_BACKEND_URL || "http://localhost:5005";
+console.log('backendHost', backendHost)
 export default backendHost;
