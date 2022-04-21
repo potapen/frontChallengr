bb3805f9ad4072c796b1a72262dbfc2bbadcfd58
Alexandre Halley <alexandre.m.halley@gmail.com>
2022-04-21 12:07:33 +0200

    revamp challenges list

---
+++ src/components/challenges/ChallengeDeleter.js
@@ -0,0 +1,49 @@
+import React from "react";
+import axios from "axios";
+
+import DeleteIcon from "@mui/icons-material/Delete";
+
+import backendHost from "../../utils/backendHost";
+import ConfirmationDialog from "../../interactivity/ConfirmationDialog.js";
+import { IconButton } from "@mui/material";
+
+function ChallengeDeleter({
+  challenge,
+  updateChallengesList,
+  updateFullChallengesList,
+}) {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const storedToken = localStorage.getItem("authToken");
+
+  const deleteChallenge = async () => {
+    await axios.delete(`${backendHost}/api/challenges/${challenge._id}`, {
+      headers: {
+        Authorization: `Bearer ${storedToken}`,
+      },
+    });
+    updateChallengesList();
+    updateFullChallengesList();
+  };
+
+  return (
+    <>
+      <IconButton aria-label="delete" onClick={handleClickOpen}>
+        <DeleteIcon />
+      </IconButton>
+      <ConfirmationDialog
+        title={`Delete challenge ?`}
+        subtitle={`This will irretrievably impact your stats`}
+        onAccept={deleteChallenge}
+        open={open}
+        setOpen={setOpen}
+      />
+    </>
+  );
+}
+
+export default ChallengeDeleter;
